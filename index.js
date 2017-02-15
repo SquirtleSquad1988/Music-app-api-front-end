0e9af1e42aff76cbed1263a48b851435dfff2a5e
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-02-14 21:30:03 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
+// generic change
+// load manifests
+// scripts
+require('./assets/scripts/index.js');
+
+// styles
+require('./assets/styles/index.scss');
