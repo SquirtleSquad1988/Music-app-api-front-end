c74924381d35adfe028bdcb0f39406aaf80aab77
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-02-17 12:45:33 -0500

    save point

--- assets/scripts/album-stuff/api.js
+++ assets/scripts/album-stuff/api.js
@@ -6,9 +6,9 @@ const store = require('../store.js');
 const createAlbum = function (data) {
   return $.ajax({
     url: config.apiOrigin + '/albums',
-    // headers: {
-    //   Authorization: `Token token=${store.user.token}`
-    // },
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    },
     method: 'POST',
     data,
   });
